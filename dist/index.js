09585f61a1a6f106407b9ee4f09b93099ad53ed1
anuruddhhit07 <88274721+anuruddhhit07@users.noreply.github.com>
2024-05-03 15:35:24 +0900

    12132

--- dist/index.js
+++ dist/index.js
@@ -12684,12 +12684,12 @@ class testdatamodule {
     }
 }
 
-new testdatamodule(20);
+const indobj = new testdatamodule(20);
 // console.log(indobj);
-// const sma= indobj.getSMAForPeriod(10)
-// console.log(sma);
-// console.log(sma.length);
-// console.log(sma.slice(0,10));
-// console.log(sma[0]);
+const sma = indobj.getSMAForPeriod(10);
+console.log(sma);
+console.log(sma.length);
+console.log(sma.slice(0, 10));
+console.log(sma[0]);
 
 export { testdatamodule };
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ export {testdatamodule}
 
 const indobj= new testdatamodule(20)
 // console.log(indobj);
-// const sma= indobj.getSMAForPeriod(10)
-// console.log(sma);
-// console.log(sma.length);
-// console.log(sma.slice(0,10));
-// console.log(sma[0]);
+const sma= indobj.getSMAForPeriod(10)
+console.log(sma);
+console.log(sma.length);
+console.log(sma.slice(0,10));
+console.log(sma[0]);
